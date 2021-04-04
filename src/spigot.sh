c3f6cce46a394db121b1d53cf06f0f5d06f9a5ed
samupro-dev <72470573+samupro-dev@users.noreply.github.com>
2021-04-04 12:26:33 +0200

    Update spigot.sh

--- src/spigot.sh
+++ src/spigot.sh
@@ -941,7 +941,7 @@ function glowstonesuccess {
   echo -e "${LIGHT_PURPLE}_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_${COLOR_NULL}"
   echo -e "${LIGHT_GREEN} Your server was successfully installed!\n   ${CYAN}* Version: ${WHITE}Glowstone ${GLOWSTONEVERSIONSEL}\n   ${CYAN}* Location: ${WHITE}${glowstonefolder}\n   ${CYAN}* RAM: ${WHITE}${glowstonemem}M\n   ${CYAN}* Port: ${WHITE}${glowstoneport} ${COLOR_NULL}"
   echo -e "${LIGHT_PURPLE}_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_${COLOR_NULL}"
-  exit
+  exit 1
 }
 
 spigot
