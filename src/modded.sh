7173b1210ed333f792920fd64d7598ca53d2cc17
samupro-dev <72470573+samupro-dev@users.noreply.github.com>
2021-04-04 16:14:50 +0200

    Update modded.sh

--- src/modded.sh
+++ src/modded.sh
@@ -84,7 +84,7 @@ function magmasuccess {
   echo -e "${LIGHT_PURPLE}_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_${COLOR_NULL}"
   echo -e "${LIGHT_GREEN} Your server was successfully installed!\n   ${CYAN}* Version: ${WHITE}Magma\n   ${CYAN}* Location: ${WHITE}${moddedfolder}\n   ${CYAN}* RAM: ${WHITE}${moddedmem}M\n   ${CYAN}* Port: ${WHITE}${moddedport} ${COLOR_NULL}"
   echo -e "${LIGHT_PURPLE}_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_/-/_${COLOR_NULL}"
-  exit 1
+  exit
 }
 
 modded
